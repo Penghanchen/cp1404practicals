a0cb16d04a0539d1e107bd0bc300a3a8be8b672b
penghan chen <penghan.chen@my.jcu.edu.au>
2025-06-15 22:05:26 +0800

    Add starter files

---
+++ prac_04/total_income.py
@@ -0,0 +1,27 @@
+"""
+CP1404/CP5632 Practical
+Starter code for cumulative total income program
+"""
+
+
+def main():
+    incomes = []
+    num_months = int(input("How many months? "))
+
+    for month in range(1, num_months + 1):
+        income = float(input(f"Enter income for month {month}: "))
+        incomes.append(income)
+
+    print_income_report(incomes)
+
+
+def print_income_report(incomes):
+    print("\nIncome Report\n-------------")
+    total = 0
+    for month, income in enumerate(incomes, start=1):
+        total += income
+        print(f"Month {month:2} - Income: ${income:10.2f}  Total: ${total:10.2f}")
+
+
+if __name__ == "__main__":
+    main()
